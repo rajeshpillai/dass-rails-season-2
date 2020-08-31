ecbe1077de7d4f37becbd5fe90fe77f4b11ab6e0
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-08-31 11:17:15 +0530

    Cleanup

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,13 +2,12 @@ class PublicController < ApplicationController
   def index
     # @posts = Post.all
     @posts = Post.includes(:tags, :category).order(created_at: :desc)
+    @categories = Category.all
   end
 
   # Search input for tag
   def tags 
     tag = params[:tag]
     @posts =  Tag.find_by(:name => tag).posts     #Tag.where(name: tag)
-    # render json: @posts
-    
   end
 end
